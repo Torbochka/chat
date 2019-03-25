53ec6079a567817bdeb02e4fc4590463b1f93641
Alexander Tobek <sasha.torbek@yandex.ru>
2019-03-25 22:18:18 +0300

    add mlab bd

--- src/server/settings.js
+++ src/server/settings.js
@@ -1,6 +1,7 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/chat', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/chat', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://user:user@cluster0-ofsf7.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
 let db = mongoose.connection;
 
